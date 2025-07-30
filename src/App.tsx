f2548113ec49b4132b2bd958e68f951043a887f1
Younhee <younhee.lee@operto.com>
2025-07-29 23:41:55 -0700

    adding roles when user is created

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,28 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import { signUp, type SignUpInput } from "aws-amplify/auth";
 
 const App = () => {
+  const services = {
+    async handleSignUp(input: SignUpInput) {
+      const userAttributes = {
+        ...(input.options?.userAttributes ?? {}),
+        "custom:roles": "staff",
+      };
+
+      return signUp({
+        ...input,
+        options: {
+          ...input.options,
+          userAttributes,
+        },
+      });
+    },
+  };
+
   return (
-    <Authenticator variation="modal">
+    <Authenticator variation="modal" services={services}>
       {({ user }) => (
         <Provider store={store}>
           <BrowserRouter>
